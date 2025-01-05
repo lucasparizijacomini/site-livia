ec009a4c21ef5b5bdf26ed44c0eb577c5b8706d7
Lucas Jacomini <lucasparizijacomini@gmail.com>
2025-01-05 13:14:08 -0300

    Implementação novas funcionalidades

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LocalizacaoComponent } from './pages/localizacao/localizacao.component';
 
 export const routes: Routes = [
-  {
-    path: '**',
-    redirectTo: '',
-  },
   {
     path: '',
     pathMatch: 'full',
     component: HomeComponent,
+  },
+  {
+    path: 'localizacao',
+    component: LocalizacaoComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  linkwppSite: string;
+  arrayBeneficios: string[];
 
+  constructor() {
+    this.linkwppSite =
+      'https://wa.me/16993493657/?text=Olá, vim através do site e gostaria de mais informações.';
+
+    this.arrayBeneficios = [
+      'APOSENTADORIAS',
+      'AUXILIO DOENÇA',
+      'APOSENTADORIA POR INVALIDEZ',
+      'SALÁRIO MATERNIDADE',
+      'PENSÃO POR MORTE',
+      'BPC/LOAS',
+      'PLANEJAMENTO PREVIDENCIARIO',
+      'AUXÍLIO ACIDENTE',
+      'AUXÍLIO RECLUSÃO',
+    ];
+  }
 }
